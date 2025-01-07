ec9aaf78ca0ce88c01779b5832ed773afa590f1e
Usman Hakam <usmanalidev62@gmail.com>
2025-01-07 19:06:24 +0500

    Blouetooth connectivity is implemented and tested

--- src/components/DeviceItem.tsx
+++ src/components/DeviceItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, TouchableOpacity, View, ActivityIndicator} from 'react-native';
+import {Text, TouchableOpacity, View} from 'react-native';
 import {Device} from 'react-native-ble-plx';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
